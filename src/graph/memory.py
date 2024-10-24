b27aabadd2ed45b9fa2fd9b79215fb53eed1ae04
Heiya <morooka.akira.inon29@gmail.com>
2024-10-25 08:07:10 +0900

    fix

--- src/graph/memory.py
+++ src/graph/memory.py
@@ -2,10 +2,6 @@ from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
 
-"""
-シンプルなメモリのチュートリアル
-"""
-
 model = ChatOpenAI(model='gpt-4o')
 
 
@@ -19,6 +15,52 @@ builder.add_node('call_model', call_model)
 builder.add_edge(START, 'call_model')
 graph = builder.compile()
 
-# memoryは、compile時にcheckpointerに渡すことで、graphの各nodeの実行結果が保存される
+# checkpointerをコンパイル時に指定する
 memory = MemorySaver()
+
+# thread_idを指定する
+config = {'configurable': {'thread_id': '1'}}
 graph = builder.compile(checkpointer=memory)
+
+answer1 = graph.invoke(
+    {
+        'messages': [
+            {
+                'role': 'user',
+                'content': '次に与える2つの数字に対して、計算してください [2,3]',
+            }
+        ]
+    },
+    config,
+)
+answer2_1 = graph.invoke(
+    {
+        'messages': [
+            {
+                'role': 'user',
+                'content': '与えられた数字の合計は？',
+            }
+        ]
+    },
+    config,
+)
+print('復元前の回答: ', answer2_1['messages'][-1].content)
+
+# 各ステップのチェックポイントを取得
+all_states = []
+for state in graph.get_state_history(config):
+    all_states.append(state)
+
+answer2_2 = graph.invoke(
+    {
+        'messages': [
+            {
+                'role': 'user',
+                'content': '与えられた数字を掛け算してください',
+            }
+        ]
+    },
+    all_states[3].config,
+)
+print('--------------------------------')
+print('復元後の回答: ', answer2_2['messages'][-1].content)
